0a8d3c861c9ee3b42431087f63640fe3251bf3fd
Travis Wright <21212370+DefinitelyTrav@users.noreply.github.com>
2020-03-05 17:49:40 -0500

    Added sequences

--- spongebob-controllerpants.ino
+++ spongebob-controllerpants.ino
@@ -1,4 +1,6 @@
 #include <XInput.h>
+#define LOOP_AFTER_LAST 1
+#define REPEAT_AFTER_LAST 2
 
 const int LED_PIN           =  0;
 const int SWITCH_PIN        =  1;
@@ -32,12 +34,37 @@ bool FIRE_BOOL_LAST      =  HIGH;
 
 const int BUTTON_LIST_SIZE = 16;
 uint8_t BUTTON_LIST[BUTTON_LIST_SIZE] = {BUTTON_A, BUTTON_B, BUTTON_Y, BUTTON_RB, TRIGGER_RIGHT, BUTTON_R3, BUTTON_L3, DPAD_UP, DPAD_DOWN, DPAD_LEFT, DPAD_RIGHT, BUTTON_BACK, BUTTON_START, TRIGGER_LEFT, BUTTON_LB, BUTTON_X};
-bool BUTTON_BOOL_LIST[BUTTON_LIST_SIZE] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
+//bool BUTTON_BOOL_LIST[BUTTON_LIST_SIZE] = {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false};
 
 bool CONFIG_MODE = false;
+bool CONFIG_MODE_LAST = false;
 int BUTTON_LIST_POS = 0;
 
 bool STICK_SWAP = false;
+
+int SEQUENCE_POS = 0;
+int SEQUENCE_SIZE = 0;                // Current size of the sequnce
+const int SEQUENCE_MAX = 5;           // Maximum steps in the sequence
+int SEQUENCE_BEHAVIOR = LOOP_AFTER_LAST;
+
+bool SEQUENCE_OF_BUTTONS[BUTTON_LIST_SIZE][SEQUENCE_MAX] = {
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+  {false, false, false, false, false},
+};
 
 void setup() {
   // put your setup code here, to run once:
@@ -68,6 +95,17 @@ void loop() {
   // Configuration mode
   CONFIG_MODE = SWITCH_BOOL;
 
+  // Reset pertinent values when CONFIG_MODE toggles
+  if (CONFIG_MODE != CONFIG_MODE_LAST){
+    // Reset button menu position
+    BUTTON_LIST_POS = 0;
+
+    // Reset sequence menu position
+    SEQUENCE_POS = 0;
+  }
+
+  // Allow editing of buttons while in CONFIG_MODE
+  // make and send inputs to computer out of CONFIG_MODE
   if (CONFIG_MODE) {
     // If configuration mode is enabled
     // use up and down to change buttons...
@@ -77,7 +115,6 @@ void loop() {
       } else {
         BUTTON_LIST_POS = BUTTON_LIST_SIZE - 1;
       }
-      //Serial.println(BUTTON_LIST_POS);
     }
     if (DOWN_BOOL && !DOWN_BOOL_LAST) {
       if (BUTTON_LIST_POS < BUTTON_LIST_SIZE - 1) {
@@ -85,37 +122,95 @@ void loop() {
       } else {
         BUTTON_LIST_POS = 0;
       }
-      //Serial.println(BUTTON_LIST_POS);
+    }
+
+    // left and right to edit sequence...
+    if (LEFT_BOOL && !LEFT_BOOL_LAST){  // Cap sequence at zero instead of wrapping.
+      if (SEQUENCE_POS > 0) {
+        SEQUENCE_POS--;
+        SEQUENCE_SIZE--;
+      } else {
+        SEQUENCE_POS = 0;
+        SEQUENCE_SIZE = 0;
+      }
+    }
+    if (RIGHT_BOOL && !RIGHT_BOOL_LAST){  // Cap sequence at max instead of wrapping.
+      if (SEQUENCE_POS < SEQUENCE_MAX - 1) {
+        SEQUENCE_POS++;
+        SEQUENCE_SIZE++;
+        BUTTON_LIST_POS = 0;
+      } else {
+        SEQUENCE_POS = SEQUENCE_MAX - 1;
+        SEQUENCE_SIZE = SEQUENCE_MAX - 1;
+        BUTTON_LIST_POS = 0;
+      }
     }
     
-    // use FIRE to toggle buttons...
+    // use FIRE to toggle buttons in the appropriate sequence...
     if (FIRE_BOOL && !FIRE_BOOL_LAST) {
-      BUTTON_BOOL_LIST[BUTTON_LIST_POS] = !BUTTON_BOOL_LIST[BUTTON_LIST_POS];
+      SEQUENCE_OF_BUTTONS[BUTTON_LIST_POS][SEQUENCE_POS] = !SEQUENCE_OF_BUTTONS[BUTTON_LIST_POS][SEQUENCE_POS];
+
+      // Debug array on exit
+      /*
+      if (CONFIG_MODE_LAST) {
+        for (int i = 0; i < BUTTON_LIST_SIZE; i++) {
+          for (int j = 0; j < SEQUENCE_MAX; j++) {
+            if (SEQUENCE_OF_BUTTONS[i][j]) {
+              Serial.print("TRUE ");
+            } else {
+              Serial.print("FALSE");
+            }
+            Serial.print(" ");
+          }
+          Serial.println();
+        }
+      }
+      */
     }
 
     // Send button press to the computer so the player
     // knows what button was activated.
     XInput.setButton(BUTTON_LIST[BUTTON_LIST_POS], FIRE_BOOL);
     XInput.send();
+
+    // Press reset to change sequence behavior 
+    // hold reset to reset everything.
+    // TODO
     
-    // ...left and right to edit sequence.
-    // If reset is also held...
-    // ...up and down to save to memory...
-    // ...left and right to select memory...
   } else {
     // If configuration mode is set to false...
-    // reset swaps stick...
-    if (RESET_BOOL && !RESET_BOOL_LAST) {
-      STICK_SWAP = !STICK_SWAP;
-    }
-    // send controller inputs to the computer
-    // Set XInput values
+    // set XInput values...
     if (FIRE_BOOL) {
-      for (int i = 0; i <= BUTTON_LIST_SIZE-1; i++) {
-        XInput.setButton(BUTTON_LIST[i], BUTTON_BOOL_LIST[i]);
+      for (int i = 0; i <= BUTTON_LIST_SIZE - 1; i++) {
+        XInput.setButton(BUTTON_LIST[i], SEQUENCE_OF_BUTTONS[i][SEQUENCE_POS]);
       }
     } else if (FIRE_BOOL_LAST) {
       XInput.releaseAll();
+    }
+    
+    // increment sequence...
+    if (!FIRE_BOOL && FIRE_BOOL_LAST) {
+      // Loop
+      if (SEQUENCE_BEHAVIOR == LOOP_AFTER_LAST) {
+        if (SEQUENCE_POS < SEQUENCE_SIZE) {
+          SEQUENCE_POS++;
+        } else {
+          SEQUENCE_POS = 0;
+        }
+      }
+      // Repeat last
+      if (SEQUENCE_BEHAVIOR == REPEAT_AFTER_LAST) {
+        if (SEQUENCE_POS < SEQUENCE_SIZE) {
+          SEQUENCE_POS++;
+        } else {
+          SEQUENCE_POS = SEQUENCE_SIZE;
+        }
+      }
+    }
+    
+    // reset swaps stick...
+    if (RESET_BOOL && !RESET_BOOL_LAST) {
+      STICK_SWAP = !STICK_SWAP;
     }
     if (!STICK_SWAP) {
       // Left stick is enabled
@@ -127,17 +222,14 @@ void loop() {
       XInput.setJoystick(JOY_RIGHT, UP_BOOL, DOWN_BOOL, LEFT_BOOL, RIGHT_BOOL);
     }
   
-    // Send control data to the computer
+    // send control data to the computer
     XInput.send();
-
-    // Reset button menu position
-    BUTTON_LIST_POS = 0;
   }
 
   // Control the LED
   digitalWrite(LED_PIN, SWITCH_BOOL);
 
-  // Store last values for debouncing.
+  // Store last values for state change.
   RESET_BOOL_LAST     =  RESET_BOOL;
   
   DOWN_BOOL_LAST      =  DOWN_BOOL;
@@ -146,5 +238,9 @@ void loop() {
   UP_BOOL_LAST        =  UP_BOOL;
   
   FIRE_BOOL_LAST      =  FIRE_BOOL;
+
+  CONFIG_MODE_LAST    = CONFIG_MODE;
+
+  // Delay for debounce.
   delay(10);
 }
